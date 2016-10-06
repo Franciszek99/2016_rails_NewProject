a7577357397195919ff731dfa5b4bd7021f28d33
Franciszek Milczarski <franko.gen@gmail.com>
2016-10-06 06:26:12 +0000

    change Gemfile, but not help, run migration with problems and remove limits

--- db/migrate/20161006053348_create_users.rb
+++ db/migrate/20161006053348_create_users.rb
@@ -1,18 +1,20 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def up 
-    create_table :users do |t|
-      t.column "first_name",:string, limit => 25
-      t.string "last_name", limit => 50
-      t.string "email", :default => '', :null=> false
-      t.string "password", limit => 50
+      
+      create_table :users do |t|
+      t.column "first_name",:string
+      t.string "last_name"
+      t.string "email", :default => '', :null => false
+      t.string "password"
       
       t.timestamps
       #t.datetime "create_at"
       #t.datetime "update_at"
     end
   end
-  
-  def down
-    drop_table :users
-  end
+    
+    def down
+     drop_table :users
+    end
+    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161006053348) do
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email",      default: "", null: false
+    t.string   "password"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
 
 end
