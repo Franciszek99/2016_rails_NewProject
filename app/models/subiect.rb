2f5c681ed98bfd26f72dd2c8d45f21578ac0fb64
Franciszek Milczarski <franko.gen@gmail.com>
2016-10-06 07:03:29 +0000

    add migrations pages, subject and sections

---
+++ app/models/subiect.rb
@@ -0,0 +1,2 @@
+class Subiect < ApplicationRecord
+end
---
+++ test/models/subiect_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SubiectTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
